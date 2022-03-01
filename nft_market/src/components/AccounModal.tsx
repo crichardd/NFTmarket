04004db8e164ef8d44593c50cd8d3a8c9a61ed30
foam60 <antoine.mousse@gmail.com>
2022-03-01 17:13:52 +0100

    update

--- nft_market/src/components/AccounModal.tsx
+++ nft_market/src/components/AccounModal.tsx
@@ -132,6 +132,90 @@ import {
                 </Link>
               </Flex>
             </Box>
+
+        {/* Profile Info */}
+
+            <Box
+              borderRadius="3xl"
+              border="1px"
+              borderStyle="solid"
+              borderColor="gray.600"
+              px={5}
+              pt={4}
+              pb={2}
+              mb={3}
+            >
+              <Flex justifyContent="space-between" alignItems="center" mb={3}>
+                <Text color="gray.400" fontSize="sm">
+                  My informations
+                </Text>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  borderColor="blue.800"
+                  borderRadius="3xl"
+                  color="blue.500"
+                  fontSize="13px"
+                  fontWeight="normal"
+                  px={2}
+                  height="26px"
+                  _hover={{
+                    background: "none",
+                    borderColor: "blue.300",
+                    textDecoration: "underline",
+                  }}
+                  onClick={handleDeactivateAccount}
+                >
+                  Save
+                </Button>
+              </Flex>
+              <Flex alignItems="center" mt={2} mb={4} lineHeight={1}>
+                <Text
+                  color="white"
+                  fontSize="xl"
+                  fontWeight="semibold"
+                  ml="2"
+                  lineHeight="1.1"
+                >
+                  {account &&
+                    `${account.slice(0, 6)}...${account.slice(
+                      account.length - 4,
+                      account.length
+                    )}`}
+                </Text>
+              </Flex>
+              <Flex alignContent="center" m={3}>
+                <Button
+                  variant="link"
+                  color="gray.400"
+                  fontWeight="normal"
+                  fontSize="sm"
+                  _hover={{
+                    textDecoration: "none",
+                    color: "whiteAlpha.800",
+                  }}
+                >
+                  <CopyIcon mr={1} />
+                  Copy Address
+                </Button>
+                <Link
+                  fontSize="sm"
+                  display="flex"
+                  alignItems="center"
+                  href={`https://ropsten.etherscan.io/address/${account}`}
+                  isExternal
+                  color="gray.400"
+                  ml={6}
+                  _hover={{
+                    color: "whiteAlpha.800",
+                    textDecoration: "underline",
+                  }}
+                >
+                  <ExternalLinkIcon mr={1} />
+                  View on Explorer
+                </Link>
+              </Flex>
+            </Box>
           </ModalBody>
   
           <ModalFooter
